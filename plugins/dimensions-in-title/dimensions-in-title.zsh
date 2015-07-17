caeb84e073e828e45a1396c1d8d565d69dcf344a
Jordan Klassen <jordan@klassen.me.uk>
2015-07-16 22:42:04 -0700

    update dimensions script
    Thu Jul 16 22:42:04 PDT 2015

--- plugins/dimensions-in-title/dimensions-in-title.zsh
+++ plugins/dimensions-in-title/dimensions-in-title.zsh
@@ -1,16 +1,19 @@
 
 # TODO: use TRAPWINCH instead (trap window change)
 # currenly conflicts with zsh-async
-TMOUT=1
 
-__columns_in_title_last_dimensions="$COLUMNS:$LINES"
+_dimensions_in_title_last_dimensions="$COLUMNS:$LINES"
 
-TRAPALRM() {
+_dimensions_in_title_print() {
   # tell the terminal we are setting the title
   print -Pn "\e]0;"
 
-  [[ "$COLUMNS:$LINES" != "${__columns_in_title_last_dimensions}" ]] && print -Pn "[$COLUMNS:$LINES] "
-  __columns_in_title_last_dimensions="$COLUMNS:$LINES"
+  if [[ "$COLUMNS:$LINES" != "${_dimensions_in_title_last_dimensions}" ]] ; then
+    print -Pn "[$COLUMNS:$LINES] "
+    _dimensions_in_title_debounce_time="$(( $(date +%s) + 1 ))"
+    ( ( sleep 2; _dimensions_in_title_clear ) &)
+  fi
+  _dimensions_in_title_last_dimensions="$COLUMNS:$LINES"
 
   ### rest of the pure prompt's title
 
@@ -20,3 +23,21 @@ TRAPALRM() {
   # shows the full path in the title
   print -Pn "%~\a"
 }
+
+_dimensions_in_title_debounce_time=
+_dimensions_in_title_clear() {
+  if [[ -n "_dimensions_in_title_debounce_time" ]] && (( $(date +%s) > _dimensions_in_title_debounce_time )); then
+    kill -WINCH $$
+  fi
+}
+
+_dimensions_in_title_precmd() {
+  trap "_dimensions_in_title_print" WINCH
+  # trap "_dimensions_in_title_print" USR2
+}
+add-zsh-hook precmd _dimensions_in_title_precmd
+
+# TMOUT=1
+# TRAPALRM() {
+#   _dimensions_in_title_print
+# }
