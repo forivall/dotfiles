575c66b4ee58b829b35e510c972c63f12a4b5927
Jordan Klassen <jordan@klassen.me.uk>
2015-08-25 20:01:53 -0700

    Add oneliner plugin
    Tue Aug 25 20:01:54 PDT 2015

---
+++ plugins/oneliner/oneliner.plugin.zsh
@@ -0,0 +1,59 @@
+__zsh_oneliner_plugin_location=$0:A
+__zsh_oneliner_plugin_location=${__zsh_oneliner_plugin_location%/*}
+
+
+oneliner() {
+  local cmd="${1}"
+  if [[ -z "${cmd}" ]]; then
+    echo "usage: oneliner [create|...(todo!)]"
+    return 1
+  fi
+
+  shift
+
+  if functions "oneliner-${cmd}" > /dev/null ; then
+    "oneliner-${cmd}" "${@}"
+  else
+    echo "oneliner: command not found: ${cmd}"
+  fi
+}
+alias oneliner="nocorrect noglob oneliner"
+
+oneliner-create() {
+  local name="${1}"
+  if [[ -z "${name}" ]]; then
+    echo "usage: oneliner create <name> <command>"
+    return 1
+  fi
+
+  shift
+
+  code="${1}"
+
+  if [[ -z "$code" ]]; then
+    echo -n "> "
+    read code
+  fi
+
+  echo "$code" > "$__zsh_oneliner_plugin_location/$name"
+
+  autoload -U "$name"
+
+  echo "Successfully created oneliner \"$name\""
+}
+alias oneliner-create="nocorrect noglob oneliner-create"
+
+oneliner-init() {
+  local s
+  local name
+  for s in "$__zsh_oneliner_plugin_location/"* ; do
+    name="${s##*/}"
+    if [[ "$name" != "oneliner.plugin.zsh" ]]; then
+      autoload -U "$name"
+    fi
+  done
+}
+
+if [[ -z "$NO_ONELINER_AUTOINIT" ]]; then
+  oneliner init
+fi
