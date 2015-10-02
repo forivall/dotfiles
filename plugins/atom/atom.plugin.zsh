c8dd07563f70327a4bbc15d2b4f7f855e7bbd57c
Jordan Klassen <jordan@klassen.me.uk>
2015-10-01 18:14:14 -0700

    Update apm-install-all

--- plugins/atom/atom.plugin.zsh
+++ plugins/atom/atom.plugin.zsh
@@ -1,5 +1,5 @@
 apm-install-all () {
   comm -13 --nocheck-order \
-    <(apm list --bare --installed | cut -f1 -d@) \
+    <(apm list --bare --installed | sort | cut -f1 -d@) \
     <(< "$1" cut -f1 -d@) | xargs apm install
 }
