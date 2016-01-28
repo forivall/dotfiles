63386dbf266f9783ab2465054f08be1d9cbea834
Jordan Klassen <jordan@klassen.me.uk>
2016-01-27 18:25:33 -0800

    Update atom config, update other dotfiles
    Wed Jan 27 18:25:33 PST 2016

--- plugins/trash/trash.plugin.zsh
+++ plugins/trash/trash.plugin.zsh
@@ -3,7 +3,7 @@ if [ -z "${KDE_SESSION_UID+x}" ] ; then
   alias rmsu="sudo gvfs-trash"
 else
   if whence kioclient5 2>/dev/null >/dev/null ; then
-    trash() { kioclient5 move "$@" "trash:/" ; }
+    trash() { kioclient5 move "$@" "trash:/" 2> >(grep -v "Invalid Context" 1>&2) ; }
     trashsu() { sudo kioclient5 move "$@" "trash:/" ; }
   else
     trash() { kioclient move "$@" "trash:/" ; }
