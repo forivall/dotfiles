70c056c45d9af5ee685d2f486012c2b28b6d05ee
Jordan Klassen <jordan@klassen.me.uk>
2016-04-06 11:29:22 -0700

    add homebrew package install script

---
+++ atom/brew.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+brew install coreutils findutils gnu-tar gnu-sed gawk gnutls gnu-indent gnu-getopt
+brew install most colordiff trash
+brew install git hub git-extras
+brew install jq httpie
+brew install flow shellcheck pandoc
+brew install mongodb caskroom/cask/robomongo
+brew install diffuse homebrew/gui/meld
