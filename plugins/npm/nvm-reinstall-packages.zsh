86a2de766fa7ccf8ee16fb50f52cda7121709688
EJ Klassen <forivall@gmail.com>
2017-02-01 16:43:01 -0800

    refactor(nvm-reinstall-packages): use npm commands to get dirs

--- plugins/npm/nvm-reinstall-packages.zsh
+++ plugins/npm/nvm-reinstall-packages.zsh
@@ -1,4 +1,20 @@
 #!/usr/bin/env zsh
+whence nvm > /dev/null && nvm current || . "$NVM_DIR/nvm.sh"
+
+list-packages () {
+  (
+  if [[ -n "$1" ]] ; then
+    nvm use --silent "$1"
+  fi
+  local prefix="$(npm -g prefix)"
+  # npm ls -g --parseable --depth=0 | sed -E "s|$prefix/lib(/node_modules/)?||g" | sort
+  cd "$prefix/lib/node_modules"
+  echo ${(@F)$(echo *(/))}
+  )
+}
+
 set -e
-. "$NVM_DIR/nvm.sh"
-npm i -g $(comm -13 <(cd "$(npm -g prefix)/lib/node_modules"; echo ${(@F)$(echo *(/))}) <(nvm use --silent $1 && cd "$(npm -g prefix)/lib/node_modules" && echo ${(@F)$(echo *(/))}))
+nvm use $1
+packages=($(comm -13 <(list-packages) <(list-packages $2)))
+echo npm i -g $packages
+npm i -g $packages
