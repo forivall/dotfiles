372136c2187974165c4b14515f4c4eb1a8bc8f48
Emily Marigold Klassen <forivall@gmail.com>
2017-10-18 18:25:15 -0700

    chore: add vscode settings

---
+++ vscode/backup.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env zsh
+
+cp ~/"Library/Application Support/Code/User/settings.json" .
+cp ~/"Library/Application Support/Code/User/keybindings.json" .
+cp -r ~/"Library/Application Support/Code/User/snippets" .
