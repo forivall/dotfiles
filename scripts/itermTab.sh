e355108fbcd6a106bf1e1d7c886ce562919adec4
Emily Marigold Klassen <forivall@gmail.com>
2017-12-08 17:45:59 -0800

    chore: update

--- scripts/itermTab.sh
+++ scripts/itermTab.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
+real_path="`/usr/local/bin/greadlink -f "$1"`"
+
 osascript -e "
 tell application \"iTerm2\"
   tell current window
     create tab with default profile
     delay 0.2
-    set pathwithSpaces to \"$(/usr/local/bin/greadlink -f "$1")\"
+    set pathwithSpaces to \"$real_path\"
     tell current session
       write text \"cd \" & quoted form of pathwithSpaces & \"; tput reset\"
     end tell
