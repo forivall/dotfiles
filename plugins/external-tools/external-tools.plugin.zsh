b82504b5c239e6e1d7d2afe254ac6dd76b5db4c8
Emily Marigold Klassen <forivall@gmail.com>
2019-12-20 15:02:43 -0800

    cleanup

--- plugins/external-tools/external-tools.plugin.zsh
+++ plugins/external-tools/external-tools.plugin.zsh
@@ -1,2 +1,11 @@
 
-PATH="$PATH:$(dirname $0)/bin:$(dirname $0)/fabiokr-dotfiles/bin:$(dirname $0)/leshylabs-blogcode/2013-08-04-making_animated_gifs_from_the_linux_command_line"
+__zsh_external_tools_plugin__dirname=${0:A:h}
+
+path=($path $__zsh_external_tools_plugin__dirname/bin)
+
+[[ -d $__zsh_external_tools_plugin__dirname/fabiokr-dotfiles ]] && path=(
+  $path $__zsh_external_tools_plugin__dirname/fabiokr-dotfiles/bin
+)
+[[ -d $__zsh_external_tools_plugin__dirname/leshylabs-blogcode ]] && path=(
+  $path $__zsh_external_tools_plugin__dirname/leshylabs-blogcode/2013-08-04-making_animated_gifs_from_the_linux_command_line
+)
