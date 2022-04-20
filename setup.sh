c58b53e14f9ace946dd7696dbbb26773b1ba5254
Emily M Klassen <forivall@gmail.com>
2022-04-20 12:39:28 -0700

    fix htop config

--- setup.sh
+++ setup.sh
@@ -71,7 +71,7 @@ o ln -fs "$(realpath pythonhist)" ~/.pythonhist
 o mkdir -p ~/.config/glab-cli
 o ln -fs "$(realpath plugins/glab/aliases.yml)" ~/.config/glab-cli/aliases.yml
 o mkdir -p ~/.config/htop
-o ln -fs "$(realpath htop)" ~/.config/htop/htoprc
+o ln -fs "$(realpath htoprc)" ~/.config/htop/htoprc
 o ln -fs "$(realpath ctop)" ~/.ctop
 
 o ln -fs "$(realpath colordiffrc)" ~/.colordiffrc
