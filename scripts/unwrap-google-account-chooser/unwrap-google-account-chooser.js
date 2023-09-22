826ad14d437ad7c98633e1cc97af44194245dacc
Emily M Klassen <forivall@gmail.com>
2023-09-22 11:03:51 -0700

    update

--- scripts/unwrap-google-account-chooser/unwrap-google-account-chooser.js
+++ scripts/unwrap-google-account-chooser/unwrap-google-account-chooser.js
@@ -1,14 +1,20 @@
 #!/usr/bin/env node
 
-import open from 'open';
-import { URL } from 'url';
+import open from 'open'
+import { execFile } from 'child_process'
+import { URL } from 'url'
 
-const urlString = process.argv[2];
-const u = new URL(urlString);
+const urlString = process.argv[2]
+const u = new URL(urlString)
 // velja regex: ^accounts\.google\.com/AccountChooser\?(.*&)?continue=https%3A%2F%2Fmeet.google.com%2F
-const continueUrl = u.searchParams.get('continue');
+const continueUrl = u.searchParams.get('continue')
 if (continueUrl?.startsWith('https://meet.google.com')) {
-  open(continueUrl);
+  console.log(continueUrl)
+  execFile('/Applications/Google Chrome.app/Contents/MacOS/Google Chrome', [
+    '--app-id=kjgfgldnnfoeklkmfkjfagphfepbbdan',
+    `--app-launch-url-for-shortcuts-menu-item=${continueUrl}`,
+  ])
+  open(continueUrl)
 } else {
-  open(urlString);
+  open(urlString)
 }
