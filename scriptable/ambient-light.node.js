8c7aa12fa362f4128a81b545696fa878bdbc8154
Emily M Klassen <forivall@gmail.com>
2023-09-29 14:45:08 -0700

    update

--- scriptable/ambient-light.node.js
+++ scriptable/ambient-light.node.js
@@ -1,43 +1,108 @@
 #!/usr/bin/env node
 import child from 'node:child_process';
+import fs from 'node:fs';
 import http from 'node:http';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
+import userStartup from 'user-startup';
 
 const THIS_FILE = fileURLToPath(import.meta.url);
-const PORT = 45967;
-const INTERVAL = 3000;
+const SERVICE_NAME = 'com.forivall.ambient-light-scriptable';
 
-const lmutracker = path.resolve(
-  path.dirname(THIS_FILE),
-  '../util/macos-ambient-light-sensor/lmutracker',
-);
-
-let currentBrightness = 0;
-let lastUpdate = 0;
-let shutdown = false;
-
-/** @type {child.ChildProcessWithoutNullStreams} */
-let c;
-function startTracker() {
-  c = child.spawn(lmutracker, ['-w'], {
-    stdio: 'pipe',
-  });
-
-  c.stdout.on('data', (/** @type {Buffer} */ value) => {
-    lastUpdate = Date.now();
-    currentBrightness = parseFloat(value.toString().trim());
-  });
-
-  c.once('exit', (code, signal) => {
-    if (!shutdown) {
-      startTracker();
-    }
-  });
+const arg = process.argv[2];
+if (arg === 'server') {
+  server();
+} else {
+  const explicitRegister = arg === 'register';
+  const LOGFILE = `${path.dirname(THIS_FILE)}/ambient-light.log`;
+  const SERVICE_FILE = userStartup.getFile(SERVICE_NAME);
+  const registered = fs.existsSync(SERVICE_FILE);
+  const stdio = explicitRegister ? 'inherit' : 'ignore';
+  if (explicitRegister || !registered) {
+    if (explicitRegister) console.log(SERVICE_FILE, registered);
+    userStartup.remove(SERVICE_NAME);
+    userStartup.add(
+      SERVICE_NAME,
+      process.execPath,
+      [...process.execArgv, THIS_FILE, 'server'],
+      LOGFILE,
+    );
+    child.execFileSync('sleep', ['1'], { stdio: 'inherit' });
+    child.execFileSync('launchctl', ['load', SERVICE_FILE], {
+      stdio: 'inherit',
+    });
+    child.execFileSync('sleep', ['1'], { stdio: 'inherit' });
+  }
+  child.execFileSync(
+    'launchctl',
+    ['kickstart', `gui/${process.getuid()}/${SERVICE_NAME}`],
+    { stdio: 'inherit' },
+  );
+  if (!explicitRegister) {
+    spawnDetached('tail', ['-f', LOGFILE]);
+  }
 }
-startTracker();
 
-const html = () => /*html*/ `<!DOCTYPE html>
+function spawnDetached(command, args = []) {
+  const dtachSession = `/tmp/${SERVICE_NAME}.dtach`;
+  child
+    .spawn('dtach', ['-A', dtachSession, '-r', 'none', command, ...args], {
+      shell: false,
+      stdio: 'inherit',
+    })
+    .on('error', (err) => {
+      if (err.code === 'ENOENT') {
+        child
+          .spawn(command, args, {
+            detached: true,
+            shell: false,
+            stdio: 'inherit',
+          })
+          .unref();
+      }
+    })
+    .on('spawn', () => {
+      setTimeout(() => {
+        console.log('$', 'dtach -a', dtachSession);
+      });
+    })
+    .unref();
+}
+
+function server() {
+  const PORT = 45967;
+  const INTERVAL = 3000;
+
+  const lmutracker = path.resolve(
+    path.dirname(THIS_FILE),
+    '../util/macos-ambient-light-sensor/lmutracker',
+  );
+
+  let currentBrightness = 0;
+  let lastUpdate = 0;
+  let shutdown = false;
+
+  /** @type {child.ChildProcessWithoutNullStreams} */
+  let c;
+  function startTracker() {
+    c = child.spawn(lmutracker, ['-w'], {
+      stdio: 'pipe',
+    });
+
+    c.stdout.on('data', (/** @type {Buffer} */ value) => {
+      lastUpdate = Date.now();
+      currentBrightness = parseFloat(value.toString().trim());
+    });
+
+    c.once('exit', (code, signal) => {
+      if (!shutdown) {
+        startTracker();
+      }
+    });
+  }
+  startTracker();
+
+  const html = () => /*html*/ `<!DOCTYPE html>
 <head>
   <style type="text/css">
     html, body {
@@ -91,55 +156,56 @@ async function getBrightness() {
 })();</script>
 </body>`;
 
-const server = http.createServer(async (req, res) => {
-  console.log(new Date().toString(), req.method, req.url, req.headers);
-  if (req.method !== 'GET') {
-    res.statusCode = 400;
-    res.statusMessage = 'Bad Request';
-    res.end();
-    return;
-  }
-  const url = new URL(req.url, `http://localhost:${PORT}`);
-  const wait = url.searchParams.get('wait');
-  if (wait) {
-    await new Promise((resolve) =>
-      setTimeout(resolve, parseInt(wait, 10) || 0),
-    );
-  }
-  switch (url.pathname) {
-    case '/currentBrightness':
-      console.log(new Date(lastUpdate).toString(), currentBrightness);
-      res.statusCode = 200;
-      res.statusMessage = 'OK';
-      res.setHeader('Content-Type', 'application/json');
-      res.write(JSON.stringify({ currentBrightness }));
+  const server = http.createServer(async (req, res) => {
+    console.log(new Date().toString(), req.method, req.url, req.headers);
+    if (req.method !== 'GET') {
+      res.statusCode = 400;
+      res.statusMessage = 'Bad Request';
       res.end();
-      break;
-    case '/currentBrightness.html':
-      res.statusCode = 200;
-      res.statusMessage = 'OK';
-      res.setHeader('Content-Type', 'text/html');
-      res.write(html());
-      res.end();
-      break;
-    default:
-      res.statusCode = 404;
-      res.statusMessage = 'Not Found';
-      res.write('Not Found');
-      res.end();
-  }
-});
+      return;
+    }
+    const url = new URL(req.url, `http://localhost:${PORT}`);
+    const wait = url.searchParams.get('wait');
+    if (wait) {
+      await new Promise((resolve) =>
+        setTimeout(resolve, parseInt(wait, 10) || 0),
+      );
+    }
+    switch (url.pathname) {
+      case '/currentBrightness':
+        console.log(new Date(lastUpdate).toString(), currentBrightness);
+        res.statusCode = 200;
+        res.statusMessage = 'OK';
+        res.setHeader('Content-Type', 'application/json');
+        res.write(JSON.stringify({ currentBrightness }));
+        res.end();
+        break;
+      case '/currentBrightness.html':
+        res.statusCode = 200;
+        res.statusMessage = 'OK';
+        res.setHeader('Content-Type', 'text/html');
+        res.write(html());
+        res.end();
+        break;
+      default:
+        res.statusCode = 404;
+        res.statusMessage = 'Not Found';
+        res.write('Not Found');
+        res.end();
+    }
+  });
 
-server.listen(PORT, () => {
-  console.log(`listening on ${PORT}`);
-});
+  server.listen(PORT, () => {
+    console.log(`listening on ${PORT}`);
+  });
 
-/** @type {NodeJS.SignalsListener} */
-const handleTermination = (signal) => {
-  shutdown = true;
-  c.kill(signal);
-  server.close();
-  process.exitCode = { SIGINT: 128 + 2, SIGTERM: 128 + 15 }[signal] || 1;
-};
-process.once('SIGINT', handleTermination);
-process.once('SIGTERM', handleTermination);
+  /** @type {NodeJS.SignalsListener} */
+  const handleTermination = (signal) => {
+    shutdown = true;
+    c.kill(signal);
+    server.close();
+    process.exitCode = { SIGINT: 128 + 2, SIGTERM: 128 + 15 }[signal] || 1;
+  };
+  process.once('SIGINT', handleTermination);
+  process.once('SIGTERM', handleTermination);
+}
