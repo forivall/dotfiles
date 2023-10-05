273e0ce3acd711ed987e35f0a77047c7e534f22f
Emily M Klassen <forivall@gmail.com>
2023-10-05 16:46:02 -0700

    update

--- macos-sync.sh
+++ macos-sync.sh
@@ -4,7 +4,7 @@ set -e
 
 cd "$(dirname "$0")" || exit
 
-cp ~/.config/mackup/Library/Application\ Support/Code/User/*.json ./vscode
+cp ~/Library/Application\ Support/Code/User/*.json ./vscode
 cp -r ~/.config/mackup/Library/Application\ Support/Code/User/snippets ./vscode
 cp ~/.config/mackup/Library/Application\ Support/Sublime\ Merge/Packages/User/*.sublime-settings ./subl/merge
 cp ~/.config/mackup/Library/Application\ Support/Sublime\ Merge/Packages/User/Default*.sublime-keymap ./subl/merge
