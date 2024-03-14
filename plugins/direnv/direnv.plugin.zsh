cdc1cbd91d6c7e3ab46407ce49868217f72eb5e4
Emily M Klassen <forivall@gmail.com>
2024-03-14 10:14:06 -0700

    update

--- plugins/direnv/direnv.plugin.zsh
+++ plugins/direnv/direnv.plugin.zsh
@@ -1,1 +1,5 @@
 [[ -n ${functions[_direnv_hook]} ]] && _direnv_hook
+
+if [[ -v HOMEBREW_PREFIX ]] && [[ -d $HOMEBREW_PREFIX/lib/node_modules/update-nodejs-notifier ]]; then
+  node -e "require('$HOMEBREW_PREFIX/lib/node_modules/update-nodejs-notifier').updateNodejsNotifier({ notSupported: false, stableMajor: false, stableMinor: true, stablePatch: true, notify: ({message}) => console.log(message) })"
+fi
