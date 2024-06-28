391d609127ab675c45b47c26caaca5d747e8a9d6
Emily M Klassen <forivall@users.noreply.github.com>
2024-06-28 12:54:55 -0700

    update

--- plugins/local/local.plugin.zsh
+++ plugins/local/local.plugin.zsh
@@ -6,3 +6,5 @@ PATH="$PATH:${__zsh_forivall_local_plugin_location}/bin"
 
 [[ -e $__zsh_forivall_local_plugin_location/local.source.zsh ]] &&
   source $__zsh_forivall_local_plugin_location/local.source.zsh
+
+true
