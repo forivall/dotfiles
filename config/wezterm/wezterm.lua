f440ee5a98c07583fbcccb8118413d2c1c7fde8e
Emily M Klassen <forivall@gmail.com>
2025-11-05 17:04:53 -0800

    update

---
+++ config/wezterm/wezterm.lua
@@ -0,0 +1,49 @@
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+
+config.bold_brightens_ansi_colors = false
+
+config.colors = {
+  foreground = '#D0D2D1',
+  background = '#27292C',
+
+  cursor_bg = '#D0D2D1',
+  cursor_fg = '#27292C',
+
+  selection_bg = '#534C53',
+  selection_fg = '#D0D2D1',
+
+  ansi = {
+    '#000000', -- black (0)
+    '#e19590', -- red (1)
+    '#c8cc89', -- green (2)
+    '#edd090', -- yellow (3)
+    '#98b0c7', -- blue (4)
+    '#bba7c4', -- magenta (5)
+    '#a3c7c3', -- cyan (6)
+    '#fefefe', -- white (7)
+  },
+
+  brights = {
+    '#191815', -- black (8)
+    '#c67c78', -- red (9)
+    '#b1b574', -- green (10)
+    '#ccb071', -- yellow (11)
+    '#7a91a6', -- blue (12)
+    '#9c87a4', -- magenta (13)
+    '#84a6a3', -- cyan (14)
+    '#dddddd', -- white (15)
+  },
+}
+
+-- config.default_prog = {"/usr/local/bin/zsh"}
+config.freetype_load_target = "Light"
+config.freetype_load_flags = "NO_HINTING"
+config.font = wezterm.font("FantasqueSansM Nerd Font Mono", {})
+config.font_size=12.24
+config.line_height=1.1
+-- config.font_antialias="Subpixel"
+config.color_scheme = "Tomorrow Night"
+config.scrollback_lines = 200000
+
+return config
