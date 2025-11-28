22994db51773e6d3d06a527f59a8c8e727060d7a
Emily M Klassen <forivall@gmail.com>
2025-11-28 10:58:08 -0800

    update

--- bat/_themes.sh
+++ bat/_themes.sh
@@ -5,3 +5,4 @@ curl -OL https://github.com/chriskempson/tomorrow-theme/raw/master/textmate/Tomo
 curl -OL https://github.com/chriskempson/tomorrow-theme/raw/master/textmate/Tomorrow.tmTheme
 curl -OL https://github.com/chriskempson/base16-textmate/raw/master/Themes/base16-tomorrow.tmTheme
 curl -OL https://github.com/chriskempson/base16-textmate/raw/master/Themes/base16-tomorrow-night.tmTheme
+sd '#96989655' '#3b3d3e' base16-tomorrow-night.tmTheme
