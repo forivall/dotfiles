2e26fb399bb7b330746e89581cab0372bbc14f8b
Emily M Klassen <forivall@gmail.com>
2026-08-31 09:41:08 -0700

    update

---
+++ plugins/go/zplug.zsh
@@ -0,0 +1,15 @@
+#!/usr/bin/env zsh
+
+__filename=${0:A}
+__dirname=${__filename:h}
+
+function generate_completions() {
+  if (( ${+commands[$1]} )); then
+    local compfile="${__dirname}/_$1"
+    echo "Generating completion for $1..." >&2
+    $@ > $compfile
+  else
+    echo "Command $1 not found. Completions not generated" >&2
+  fi
+}
+generate_completions driveignore completion zsh
